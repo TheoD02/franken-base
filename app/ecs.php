a866b150f33117039618d88a8d6a72c64d3427a8
Theo D <theo.dulieu@wyzgroup.com>
2024-02-25 17:21:48 +0100

    Update key dependencies and add MethodChainingNewlineFixer to skip list
    This commit updates version of key dependencies (easy-coding-standard, phpstan, rector) in the composer.lock files and adds MethodChainingNewlineFixer to the ECS skip list. The dependencies are updated to their latest versions, for improved

--- app/ecs.php
+++ app/ecs.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ClassNotation\FinalInternalClassFixer;
+use Symplify\CodingStandard\Fixer\Spacing\MethodChainingNewlineFixer;
 
 require_once dirname(__DIR__) . '/tools/ecs/BaseECSConfig.php';
 
@@ -12,4 +13,7 @@ return BaseECSConfig::config()
         __DIR__ . '/tests',
         __DIR__ . '/modules/api/src',
     ])
-    ->withSkip([FinalInternalClassFixer::class]);
+    ->withSkip([
+        FinalInternalClassFixer::class,
+        MethodChainingNewlineFixer::class,
+    ]);
