cbd6dac237293fd50d592c125ee21284e926fbe8
Theo D <theo.dulieu@wyzgroup.com>
2024-03-01 01:49:47 +0100

    Update the CollectionType class to handle various scenarios impacting JSON encoding and decoding. This revision adds null checks and resource type checks to prevent unexpected errors. Exception handling has also been implemented to manage potential JSON encoding or decoding failures, improving error management and maintaining code integrity.

--- app/modules/api/src/Doctrine/CollectionType.php
+++ app/modules/api/src/Doctrine/CollectionType.php
@@ -5,8 +5,20 @@ declare(strict_types=1);
 namespace Module\Api\Doctrine;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\JsonType;
+use JsonException;
 use loophp\collection\Collection;
+
+use function dd;
+use function is_resource;
+use function json_decode;
+use function json_encode;
+
+use function stream_get_contents;
+
+use const JSON_PRESERVE_ZERO_FRACTION;
+use const JSON_THROW_ON_ERROR;
 
 class CollectionType extends JsonType
 {
@@ -19,13 +31,35 @@ class CollectionType extends JsonType
             throw new \InvalidArgumentException('The value must be an instance of Collection.');
         }
 
-        return json_encode($value->all(false), \JSON_THROW_ON_ERROR);
+        if ($value === null) {
+            return null;
+        }
+
+        try {
+            return json_encode($value, JSON_THROW_ON_ERROR | JSON_PRESERVE_ZERO_FRACTION);
+        } catch (JsonException $e) {
+            throw ConversionException::conversionFailedSerialization($value->all(false), 'json', $e->getMessage(), $e);
+        }
     }
 
     #[\Override]
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
-        return Collection::fromString($value);
+        if ($value === null || $value === '') {
+            return Collection::empty();
+        }
+
+        if (is_resource($value)) {
+            $value = stream_get_contents($value);
+        }
+
+        try {
+            $value = json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+        }
+
+        return Collection::fromIterable($value);
     }
 
     #[\Override]
