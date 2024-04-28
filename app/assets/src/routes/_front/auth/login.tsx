bc2eed7b56c6bbaefdce6021c41c2e26cf733655
Theo D <theo.d02290@gmail.com>
2024-04-28 19:30:43 +0200

    fix

--- app/assets/src/routes/_front/auth/login.tsx
+++
@@ -1,6 +0,0 @@
-import {createFileRoute} from "@tanstack/react-router";
-import {AuthenticationForm} from "../../../components/Auth/Login/AuthenticationForm";
-
-export const Route = createFileRoute('/_front/auth/login')({
-    component: AuthenticationForm,
-});
--- app/assets/src/routes/admin.tsx
+++
@@ -1,6 +0,0 @@
-import {createFileRoute, createRootRoute,} from "@tanstack/react-router";
-import AdminLayout from "../layouts/AdminLayout";
-
-export const Route = createFileRoute('/admin')({
-    component: AdminLayout,
-});
